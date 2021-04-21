be536d64bc3f41da588bf49c305c59d0b1903ab6
dengzhou <dengzhou@cai-inc.com>
2021-04-21 21:57:55 +0800

    初始化项目

---
+++ src/main/java/com/dengzhou/ThreadImockApplication.java
@@ -0,0 +1,13 @@
+package com.dengzhou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThreadImockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThreadImockApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dengzhou/threadimock/ThreadImockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dengzhou.threadimock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThreadImockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
